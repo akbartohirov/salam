268e4339d95eae912fb76e9120cece2ca60a9be2
akbartohirov <akb.tokhirov@gmail.com>
2022-02-17 09:01:37 +0500

    added

---
+++ client/src/Components/CatalogItem.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./CatalogItem.css";
+import { Link } from "react-router-dom";
+
+const CatalogItem = ({ item, subs }) => {
+  return (
+    <div className="catalog__item mr-2">
+      <div className="catalog__item-img mr-1">
+        <img
+          width={"60px"}
+          src="/uploads/2022-02-13T09:53:17.750Zphoto_2021-02-02_00-43-21.jpg"
+          alt=""
+        />
+      </div>
+      <div className="catalog__item-content">
+        <Link to={`/category/${item._id}`}>
+          <span
+            className="link__hover"
+            style={{ fontSize: "24px", fontWeight: 500, color: "#000" }}
+          >
+            {item.categoryName}
+          </span>
+        </Link>
+        {subs.length > 0 &&
+          subs.slice(0, 3).map((el) => (
+            <Link
+              key={el._id}
+              to={`/category/${item._id}?subcategories=${el._id}`}
+              className="mb-1"
+            >
+              <span className="link__hover" style={{ color: "#000" }}>
+                {" "}
+                {el.subCategoryName}{" "}
+              </span>
+            </Link>
+          ))}
+        {subs.length > 3 && <span> Еще {subs.length - 3}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default CatalogItem;
